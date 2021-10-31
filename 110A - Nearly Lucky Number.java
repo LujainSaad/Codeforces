612b4ffc00ce29caf58741fcad4a5d94dc3e0bfb
Lujain <79986157+LujainSaad@users.noreply.github.com>
2021-10-31 19:50:22 +0300

    Create 110A - Nearly Lucky Number.java

---
+++ 110A - Nearly Lucky Number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class NearlyLuckyNumber {
+	public static void main (String []args) throws InterruptedException 
+	{
+		Scanner s=new Scanner(System.in);
+		String S;int c=0;
+		S=s.next();
+for(int i=0;i<S.length();i++)
+	if(S.charAt(i)=='7'||S.charAt(i)=='4')
+		c++;
+    if (c==7||c==4)
+    	System.out.println("YES");
+    else System.out.println("NO");
+
+}
+}
