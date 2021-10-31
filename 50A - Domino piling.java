6c29875ea9d05caef9b073e5c7b305afdd9a9bb4
Lujain <79986157+LujainSaad@users.noreply.github.com>
2021-10-31 19:17:02 +0300

    Create 50A - Domino piling.java

---
+++ 50A - Domino piling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+public class program {
+	public static void main(String[] args) throws java.lang.Exception
+	{ double n,m;
+		Scanner S = new Scanner(System.in);	
+		n=S.nextDouble();
+		m=S.nextDouble();
+	System.out.println((int) Math.floor((n*m)/2));
+	}
+ 
+}
