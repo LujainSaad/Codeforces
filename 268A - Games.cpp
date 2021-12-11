11a3b60ac9a16e66adfcf44fae7199ed631977af
Lujain <79986157+LujainSaad@users.noreply.github.com>
2021-12-11 06:11:06 +0300

    Create 268A - Games.cpp

---
+++ 268A - Games.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n; cin>>n;
+    int s1[n],s2[n],count=0;
+ 
+    for(int i=0; i<n; i++){
+        cin>>s1[i]>>s2[i];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            
+        if(i!=j && s1[i]==s2[j])
+        count++;
+        }
+    }
+    
+    cout<<count;
+ 
+    return 0;
+}
